1e6e79057f8e8a5b208d8dcc163c357f05e74e26
RyanDBarnett <conner2468@gmail.com>
2019-06-03 10:40:43 -0600

    Move reducers into their own folders and update reducers/index.js paths

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import presidents from './presidentsReducer';
-import loading from './isLoadingReducer';
+import presidents from './presidentsReducer/';
+import loading from './isLoadingReducer/';
 import error from './errorReducer/';
 
 const rootReducer = combineReducers({
