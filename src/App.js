f486f25cac94cba38be4fed8e81cd54f2c066dc6
RyanDBarnett <conner2468@gmail.com>
2019-06-03 09:13:29 -0600

    Basic fetch presidents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      presidents: []
+    }
+  }
+
+  componentDidMount = async () => {
+    const url = 'http://localhost:3001/api/v1/presidents';
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  render = () => {
+    return (
+      <div className="App">
+        
+      </div>
+    ); 
+  }
 }
 
 export default App;
