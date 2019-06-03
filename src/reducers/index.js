1c45b4572d44181bf5b3c9a3b3a271c1237224ba
RyanDBarnett <conner2468@gmail.com>
2019-06-03 11:51:25 -0600

    Add party to reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import presidents from './presidentsReducer/';
 import loading from './isLoadingReducer/';
 import error from './errorReducer/';
+import party from './partyReducer/';
 
 const rootReducer = combineReducers({
   presidents,
   loading,
-  error
+  error,
+  party
 })
 
 export default rootReducer;
